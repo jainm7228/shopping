b88b90f7afd01ea4a4342871abbb48af02737529
jainm7228@gmail.com <user.email "jainm7228@gmail.com>
2025-03-05 13:28:56 +0530

    thunk impplemented to fectch data from an api

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "./styles/app.css";
 import App from "./App.jsx";
 import { store } from "./redux/Store.jsx";
 import { Provider } from "react-redux";
